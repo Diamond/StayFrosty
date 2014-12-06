7fec2ddb49b79cf5e1eb6b009b57e60fcd115569
Brandon Richey <diamondgfx@gmail.com>
2014-12-05 22:40:28 -0500

    Fixes

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+	public float bulletSpeed = 5.0f;
+
+	public void Fire(int direction) {
+		// Direction: 0 = up, 1 = right, 2 = down, 3 = left
+		float xvel = 0.0f;
+		float yvel = 0.0f;
+		if (direction == 0) {
+			yvel = -bulletSpeed;
+		} else if (direction == 1) {
+			xvel = bulletSpeed;
+		} else if (direction == 2) {
+			yvel = bulletSpeed;
+		} else {
+			xvel = -bulletSpeed;
+		}
+		this.rigidbody2D.velocity = new Vector2(xvel, yvel);
+	}
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float speed = 5.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		float x = (Input.GetAxis ("Horizontal") * speed) * Time.deltaTime;
+		float y = (Input.GetAxis ("Vertical") * speed) * Time.deltaTime;
+		this.transform.position += new Vector3(x, y, 0.0f);
+	}
+}
